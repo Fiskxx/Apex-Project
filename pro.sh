c168e2516c19ef2e0c07466317a506d9d6b85cad
Fiskxx <limosmuchudos@gmail.com>
2026-02-13 05:01:34 -0300

    Criar pro.sh

---
+++ pro.sh
@@ -0,0 +1,4 @@
+~/rish -c "settings put system pointer_speed 7"
+~/rish -c "settings put system touch.pressure.scale 0.001"
+~/rish -c "settings put global peak_refresh_rate 120.0"
+echo "INJEÇÃO PRO APLICADA COM SUCESSO!"
